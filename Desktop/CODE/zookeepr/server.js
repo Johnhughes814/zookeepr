a47694fe6cc5ecac5138c0381dfd4343b347b30f
Johnhughes814 <8hughes.john@gmail.com>
2022-03-21 16:49:09 -0500

    test

--- Desktop/CODE/zookeepr/server.js
+++ Desktop/CODE/zookeepr/server.js
@@ -1,3 +1,5 @@
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
@@ -8,6 +10,8 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
